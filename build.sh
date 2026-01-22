fdbdfe4581a1ccd6ee20ab9db383f9b6df7658fe
ShrekNARFU <vlasovdany2004@gmail.com>
2026-01-22 15:46:21 +0300

    Create build.sh: first script in the local CI (check stage and init report file)

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e
+
+mkdir -p report
+mkdir -p artifacts
+
+MODE="$1"
+REVISION="$(date +%d-%m-%Y_%H-%M-%S)"
+BUILD_NUM="$(find ./report -maxdepth 1 -name 'build_report_*.txt' | wc -l)"
+BUILD_NUM="$((BUILD_NUM + 1))"
+
+TIMESTAMP="${REVISION}"
+REPORT_FILE="build_report_${TIMESTAMP}.txt"
+
+echo "Building #${BUILD_NUM}, rev: ${REVISION}, mode: ${MODE}" >> "report/${REPORT_FILE}"
+
+docker build \
+    --build-arg MODE="${MODE}" \
+    --build-arg REVISION="${REVISION}" \
+    --build-arg BUILD_NUM="${BUILD_NUM}" \
+    -t iperf3:"${REVISION}_${MODE}" \
+    -f Dockerfile .
+
+docker run --rm \
+  -v "$(pwd)/artifacts:/app/deb" \
+  "iperf3:${REVISION}_${MODE}"
+echo "Готово: artifacts/iperf3-${REVISION}.deb и ${REPORT_FILE}"
